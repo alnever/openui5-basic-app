e3eb3a1f4677797b4f71e7dd8e2192abadbcacdf
Aleksei Neverov <al_neverov@prominform.com>
2019-09-20 17:59:09 +0500

    Route experiments

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,10 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/demo/basicTemplate/model/formatter"
+	"../model/formatter"
 ], function(Controller, formatter) {
 	"use strict";
 
-	return Controller.extend("sap.ui.demo.basicTemplate.controller.App", {
+	return Controller.extend("bookstore.controller.App", {
 
 		formatter: formatter,
 
